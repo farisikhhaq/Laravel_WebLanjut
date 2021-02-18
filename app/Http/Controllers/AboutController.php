0ff68a6ad716bb19349800474dca1045c1ec2b5e
Faris26 <farisikhlasulhaq@yahoo.co.id>
2021-02-18 10:01:33 +0700

    Jobsheet 2 Praktikum 2 (Controller Tahap Selesai Penambahan)

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    //
+    public function index (){
+        return "Nama : Faris Ikhlasul Haq , Nim : 1941720062";
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-// use App\Http\Controllers\AboutController;
-// use App\Http\Controllers\ArticleController;
-// use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
+// use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +17,12 @@ use App\Http\Controllers\PageController;
 |
 */
 
-Route::get('/', [PageController::class, 'index']);
+// Route::get('/', [PageController::class, 'index']);
 
-Route::prefix('/')->group(function () {
-    Route::get('about', [PageController::class, 'about']);
-    Route::get('articles/{id}', [PageController::class, 'articles']);
-});
-// Route::get('/', [HomeController::class, 'index']);
-// Route::get('/about', [AboutController::class, 'index']);
-// Route::get('/articles/{id}', [ArticleController::class, 'index']);
+// Route::prefix('/')->group(function () {
+//     Route::get('about', [PageController::class, 'about']);
+//     Route::get('articles/{id}', [PageController::class, 'articles']);
+// });
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/articles/{id}', [ArticleController::class, 'index']);
