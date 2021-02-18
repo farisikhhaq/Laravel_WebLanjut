fd34bed43a32ac4f85f4d5fb9a0aff7b74162794
Faris26 <farisikhlasulhaq@yahoo.co.id>
2021-02-18 09:45:23 +0700

    Jobsheet 2 Praktikum 1 (Route)

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    echo "Selamat Datang";
+});
+Route::get('/about', function () {
+    echo "Nama : Faris Ikhlasul Haq , Nim : 1941720062";
+});
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman dengan artikel dengan id $id";
 });
