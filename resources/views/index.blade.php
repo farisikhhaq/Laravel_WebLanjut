402a90f70d5ba0532ee3089323b49fe62fe08020
milyun <milyun.nima.shoumi@gmail.com>
2021-03-04 07:58:33 +0700

    add master layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,34 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-<!--
-
-Template 2092 Shelf
-
-http://www.tooplate.com/view/2092-shelf
-
--->
-    <title>SHELF - Your Online Bookstore</title>
-
-    <!-- load stylesheets -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">  <!-- Google web font "Open Sans" -->
-    <link rel="stylesheet" href="{{asset('assets/font-awesome-4.7.0/css/font-awesome.min.css')}}">                <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">                                      <!-- Bootstrap style -->
-    <link rel="stylesheet" href="{{asset('assets/css/tooplate-style.css')}}">                                   <!-- Templatemo style -->
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-          <![endif]-->
-</head>
-
-    <body>
-        
+@extends('layout.app')
+@section('content')
         <div class="container">
             <header class="tm-site-header">
                 <h1 class="tm-site-name">Shelf</h1>
@@ -184,5 +155,4 @@ http://www.tooplate.com/view/2092-shelf
 
         </script>             
 
-</body>
-</html>
+@endsection
